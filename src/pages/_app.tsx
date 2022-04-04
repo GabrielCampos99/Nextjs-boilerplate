d1ef9a7a8b053af4a25eab883599493031f897d2
Gabriel Campos <gabriel.campos@alicerceedu.com.br>
2022-04-04 09:28:16 -0300

    fix: styled components working

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Boilerplate</title>
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default App
