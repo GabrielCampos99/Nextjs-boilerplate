e9d5640638d6bd75c675bd40dc304bb09eef368b
Gabriel Campos <gabriel.campos@alicerceedu.com.br>
2022-04-30 09:38:50 -0300

    add next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false
+/* const nextConfig = {
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
+ */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
-const isProd = (process.env.NODE_ENV = 'production')
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = withPWA({
+  swcMinify: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
   pwa: {
     dest: 'public',
     disable: !isProd
